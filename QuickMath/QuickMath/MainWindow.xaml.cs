75073164c15447fe6ebf0c4d71a68696fe53b40a
AquaVG <vladd.golovatyuk@gmail.com>
2019-04-21 21:46:21 +0300

    Created timer for math mode

--- QuickMath/QuickMath/MainWindow.xaml.cs
+++ QuickMath/QuickMath/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QuickMath
 {
@@ -20,10 +11,48 @@ namespace QuickMath
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<CheckBox, char> relations = new Dictionary<CheckBox, char>();
+        List<char> ActiveOps = new List<char>();
+
+        System.Windows.Forms.Timer math_timer = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer memory_timer = new System.Windows.Forms.Timer();
+
+        short Seconds = 0, Minutes = 3;
         public MainWindow()
         {
             InitializeComponent();
+            relations.Add(CheckB_Add, '+');
+            relations.Add(CheckB_Rem, '-');
+            relations.Add(CheckB_Mult, '*');
+            relations.Add(CheckB_Div, '/');
+
+            math_timer.Tick += Math_timer_Tick;
+            memory_timer.Tick += Memory_timer_Tick;
+            math_timer.Interval = memory_timer.Interval = 1000;
+            Button_Click(new object(), new RoutedEventArgs());
         }
+
+        private void Memory_timer_Tick(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Math_timer_Tick(object sender, System.EventArgs e)
+        {
+            Math_Timer.Content = Seconds >= 10 ? $"0{Minutes}:{Seconds}" : $"0{Minutes}:0{Seconds}";
+
+            if (Seconds == 0 && Minutes >= 1)
+            {
+                Minutes--; Seconds = 60;
+            }
+            if (Minutes == 0 && Seconds == 0)
+            {
+                (sender as System.Windows.Forms.Timer).Enabled = false; return;
+            }
+            Seconds--;
+
+        }
+
         private bool covered = false;
         private void Link_ChangeColor(object sender, MouseEventArgs e) => (sender as Label).Foreground = !(covered = !covered) ? Brushes.Black : Brushes.LightCoral;
 
@@ -38,7 +67,20 @@ namespace QuickMath
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            ActiveOps.Clear();
+            foreach (var pair in relations)
+            {
+                if (pair.Key.IsChecked == true)
+                    ActiveOps.Add(pair.Value);
+            }
+            if (ActiveOps.Count < 2)
+            {
+                MessageBox.Show("You must select more than 2 operations.", "Error.");
+                return;
+            }
+            SecondaryMathGrid.Visibility = Visibility.Hidden;
+            MainMathGrid.Visibility = Visibility.Visible;
+            math_timer.Enabled = true;
         }
     }
 }
