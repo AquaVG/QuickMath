e9bed96d43ac205c3db1d7e68afd0c43103428be
AquaVG email = 43469966+AquaVG@users.noreply.github.com <vladd.golovatyuk@gmail.com>
2019-05-02 17:29:19 +0300

    Created MemorySkillWorker class

---
+++ QuickMath/QuickMath/MemorySkillWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMath
+{
+    class MemorySkillWorker
+    {
+        public int Right, Wrong;
+        private int Lenght;
+        public void CheckAnswer(string answer, string uanswer)
+        {
+            if (answer == uanswer) Right++;
+            else Wrong++;
+        }
+        public string Expression
+        {
+            get{
+                string expression = "";
+                for (int i = 0; i <= Lenght; i++)
+                {
+                    expression += new Random().Next(0, 10).ToString();
+                }
+                return expression;
+            }
+        }
+        public MemorySkillWorker (int lenght)
+        {
+            Lenght = lenght;
+        }
+    }
+}
