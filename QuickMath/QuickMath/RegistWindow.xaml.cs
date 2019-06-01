35813aeb7471ed3876ad0b415c3206557dd2622a
vladd.golovatyuk@gmail.com <vladd.golovatyuk@gmail.com>
2019-06-01 10:24:34 +0300

    Forgot about InfoWindow.xaml

---
+++ QuickMath/QuickMath/RegistWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuickMath
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistWindow.xaml
+    /// </summary>
+    public partial class RegistWindow : Window
+    {
+        public RegistWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
